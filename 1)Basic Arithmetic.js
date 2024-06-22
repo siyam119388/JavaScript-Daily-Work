6c4fa96d08cfa3862a8313d94455e07f8a2cfec2
Md. Ashikur Rahman Siyam <149653672+2233642642@users.noreply.github.com>
2024-06-22 09:28:39 -0700

    Create 1)Basic Arithmetic.js

---
+++ 1)Basic Arithmetic.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<p id="demo"></p>
+
+<script>
+let x = 5;
+let y = 2;
+let z = x * y;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
